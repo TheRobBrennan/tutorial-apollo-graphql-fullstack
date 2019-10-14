28b501f7cf928617da9d24b259d090f844b6128b
Rob Brennan <rob@therobbrennan.com>
2019-10-14 11:26:01 -0700

    Added custom resolver for isInCart virtual field

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -1,4 +1,12 @@
 import gql from 'graphql-tag';
+import { GET_CART_ITEMS } from './pages/cart';
+
+// Add an `isInCart` virtual field to our `Launch` type.
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
 
 /**
  * To build a client schema, we extend the types of our server schema and wrap it with the gql function. Using the extend keyword allows us to combine both schemas inside developer tooling like Apollo VSCode and Apollo DevTools.
@@ -18,4 +26,12 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+export const resolvers = {
+  Launch: {
+    // Specify a client resolver to tell Apollo Client how to resolve your virtual field on the Launch type
+    isInCart: (launch, _, { cache }) => {
+      const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+      return cartItems.includes(launch.id);
+    },
+  },
+};
