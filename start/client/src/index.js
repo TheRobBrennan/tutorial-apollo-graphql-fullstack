0519409bd116571484f8b87858bc93b1d29cb5c3
Rob Brennan <rob@therobbrennan.com>
2019-10-14 09:57:55 -0700

    Added hard-coded query example for reference

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,6 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -11,3 +12,19 @@ const client = new ApolloClient({
   cache,
   link
 });
+
+// Make your first query
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
